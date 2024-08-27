91cb3ce29da15e86b96a687c75e666c529a66e34
Siarhei Zhulym <s.zhulym@sam-solutions.com>
2024-08-27 21:37:22 +0300

    feat: add shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+// import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <h1>Init</h1>
+    <>
+      <h1>Init</h1>
+      <Button variant='outline'>Click me</Button>
+    </>
   );
 }
